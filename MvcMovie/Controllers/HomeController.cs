e832f10d2a5c8d4a1a6f9d024e66220667ba865f
bp49 <35807799+bp49@users.noreply.github.com>
2018-02-27 20:43:21 -0500

    Update

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MvcMovie.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return Redirect("Movies");
         }
 
         public IActionResult About()
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required]
+        public string Title { get; set; }
+        
+        
+        [Display(Name = "Release Date"), DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}", ApplyFormatInEditMode =false)]
+        public DateTime ReleaseDate { get; set; }
+
+        
+        [StringLength(30), Required]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+    }
+}
